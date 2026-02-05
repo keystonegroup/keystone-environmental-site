26f436ed7a277b7758b80cb5b9dee41475c13cfa
keystonegroup <accounts@ksenvironmental.us>
2026-02-05 17:21:57 -0500

    Again

--- assets/footer/footer.js
+++ assets/footer/footer.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const footerContainer = document.getElementById('footer');
   if (!footerContainer) return;
 
-  fetch('/assets/footer/footer.html')
+  fetch('assets/footer/footer.html')
     .then(response => response.text())
     .then(html => {
       footerContainer.innerHTML = html;
